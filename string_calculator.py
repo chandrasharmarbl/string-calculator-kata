afed186c95af876c9e788e869c6ae2841198e9ee
Chandra Sharma <chandrakant@incubyte.co>
2026-04-21 16:10:09 +0530

    refactor: extract _normalize from _parse method

--- string_calculator.py
+++ string_calculator.py
@@ -1,15 +1,24 @@
 class StringCalculator:
 
+    def __init__(self):
+        self.base_delimiter = ','
+
     def _parse(self, numbers: str) -> str:
-        delimiter = ','
+        return [int(num) for num in numbers.split(self.base_delimiter)]
+
+    def _normalize(self, numbers: str) -> str:
+
         if numbers.startswith("//"):
             delimiter, numbers = numbers[2:].split('\n')
-            
-        numbers = numbers.replace('\n', delimiter)
-        return [int(num) for num in numbers.split(delimiter)]
+            numbers = numbers.replace(delimiter, self.base_delimiter)
+
+        numbers = numbers.replace('\n', self.base_delimiter)
+        return numbers
 
     def add(self, input_string: str) -> int:
         if not input_string:
             return 0
         
-        return sum(self._parse(input_string))
+        normalized_string = self._normalize(input_string)
+    
+        return sum(self._parse(normalized_string))
