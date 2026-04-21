fcb33e86ebd8a8ba6aac9d8c1b93a7831ce797a8
Chandra Kant Sharma <chandrakant@incubyte.co>
2026-04-22 03:14:46 +0530

    refactor: Extract InputParser and NumberValidator classes out of StringCalculator

--- string_calculator.py
+++ string_calculator.py
@@ -1,32 +1,19 @@
 import re
 
-class StringCalculator:
-
+class InputParser:
     def __init__(self):
         self.base_delimiter = ','
-        self.max_allowed_number = 1000
-        self.call_count = 0
 
-    def get_call_count(self):
-        return self.call_count
-
-    def _parse(self, numbers: str) -> str:
-        parsed = []
-        negatives = []
-
-        for n in numbers.split(self.base_delimiter):
-            val = int(n)
-            if val < 0:
-                negatives.append(val)
-            elif val <= self.max_allowed_number:
-                parsed.append(val)
-
-        return parsed, negatives
+    def parse(self, input_string: str) -> list[int]:
+        if not input_string:
+            return []
+        
+        normalized_string = self._normalize(input_string)
+        return [int(n) for n in normalized_string.split(self.base_delimiter)]
 
     def _normalize(self, numbers: str) -> str:
-
         if numbers.startswith("//"):
-            header, numbers = numbers.split('\n')
+            header, numbers = numbers.split('\n', 1)
             delimiters = re.findall(r"\[(.*?)\]", header)
 
             if delimiters:
@@ -39,13 +26,32 @@ class StringCalculator:
         numbers = numbers.replace('\n', self.base_delimiter)
         return numbers
 
+class NumberValidator:
+    def __init__(self):
+        self.max_allowed_number = 1000
+
+    def validate_and_filter(self, numbers: list[int]) -> list[int]:
+        negatives = [n for n in numbers if n < 0]
+        if negatives:
+            raise ValueError(f"negatives not allowed: {negatives}")
+        
+        return [n for n in numbers if n <= self.max_allowed_number]
+
+class StringCalculator:
+    def __init__(self):
+        self.call_count = 0
+        self.parser = InputParser()
+        self.validator = NumberValidator()
+
+    def get_call_count(self) -> int:
+        return self.call_count
+
     def add(self, input_string: str) -> int:
         self.call_count += 1
         if not input_string:
             return 0
+            
+        parsed_numbers = self.parser.parse(input_string)
+        valid_numbers = self.validator.validate_and_filter(parsed_numbers)
         
-        normalized_string = self._normalize(input_string)
-        parsed, negatives = self._parse(normalized_string)
-        if negatives:
-            raise ValueError(f"negatives not allowed: {negatives}")
-        return sum(parsed)
+        return sum(valid_numbers)
